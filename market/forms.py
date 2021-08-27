ab2dc06f76518cd43deca018a96784ca6b699e9e
Roman Zabolotskyi <roman.zabolotskyi@gmail.com>
2021-08-27 23:21:21 +0300

    Improve form validation.

--- market/forms.py
+++ market/forms.py
@@ -1,9 +1,22 @@
+from market.models import User
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 
 
 class RegisterForm(FlaskForm):
+    # Use validate_FIELDNAME function names
+    # flask_wtf handles them automatically
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError("Username already exists!")
+
+    def validate_email_address(self, email_address_to_check):
+        user = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if user:
+            raise ValidationError("Email already exists!")
+
     username = StringField(
         label="Username:", validators=[DataRequired(), Length(min=2, max=30)]
     )
--- market/routes.py
+++ market/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from market import app, db
 from market.models import Item, User
 from market.forms import RegisterForm
@@ -34,6 +34,6 @@ def register_page():
 
     if form.errors != {}:
         for err_msg in form.errors.values():
-            print(f"There was an error: {err_msg}")
+            flash(f"There was an error: {err_msg}", category="danger")
 
     return render_template("register.html", form=form)
