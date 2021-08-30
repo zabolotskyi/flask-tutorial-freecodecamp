19eb58274beaeeb80cfb868b06025954bd16f442
Roman Zabolotskyi <roman.zabolotskyi@gmail.com>
2021-08-30 23:21:11 +0300

    Logout user.

--- market/__init__.py
+++ market/__init__.py
@@ -9,5 +9,7 @@ app.config["SECRET_KEY"] = "083cf464f2d0f6de696e36f9"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for, flash
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 from market import app, db
 from market.models import Item, User
 from market.forms import LoginForm, RegisterForm
@@ -12,6 +12,7 @@ def home_page():
 
 
 @app.route("/market")
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template("market.html", items=items)
@@ -30,6 +31,12 @@ def register_page():
 
         db.session.add(user_to_create)
         db.session.commit()
+
+        login_user(user_to_create)
+        flash(
+            f"Account created. You are logged as {user_to_create.username}",
+            category="success",
+        )
 
         return redirect(url_for("market_page"))
 
@@ -60,3 +67,11 @@ def login_page():
             )
 
     return render_template("login.html", form=form)
+
+
+@app.route("/logout")
+def logout_page():
+    logout_user()
+    flash("You have logged out!", category="info")
+
+    return redirect(url_for("home_page"))
